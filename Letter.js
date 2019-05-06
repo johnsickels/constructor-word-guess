45dc84741d91939ea7b50540ec14e1cf284237ea
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-05-06 14:08:53 -0400

    word constructor

--- Letter.js
+++ Letter.js
@@ -1,24 +1,25 @@
 // * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 var Letter = function () {
     //   * A string value to store the underlying character for the letter
-    this.string = "";
+    this.string = "a";
     //   * A boolean value that stores whether that letter has been guessed yet
-    this.boolean = false;
+    this.guessedYet = false;
     //   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
     this.reveal = function () {
-        if (this.boolean = false) {
-            this.string = "_"
+        if (this.guessedYet = false) {
+            return "_";
         } else {
-            this.string = "x"
+            return this.string;
         };
     };
     //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-    this.check = function (x) {
-        if (x = string) {
-            this.boolean = true;
+    this.check = function (letterGuess) {
+        
+        if (letterGuess = string) {
+            this.guessedYet = true;
             this.reveal();
         } else {
-            console.log("Something bad");
+            console.log("INCORRECT" + "\n~~~~~~~~~~~~~~~");
         };
     };
 }
--- Word.js
+++ Word.js
@@ -1,7 +1,29 @@
 // * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
 
-//   * An array of `new` Letter objects representing the letters of the underlying word
+let wordToGuess = "World";
+let goodGuesses = ["o", "r"];
 
-//   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+// console.log(word);
 
+var Word = function (wordParameter) {
+    //   * An array of `new` Letter objects representing the letters of the underlying word
+    this.word = wordParameter.split("");
+    //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+    this.showPlaceholders = function () {
+        let tmpString = "";
+        this.word.forEach(element => {
+            if (goodGuesses.includes(element)) {
+                tmpString += element + " ";
+            } else {
+                tmpString += "_ ";
+            }
+        });
+        console.log(tmpString);
+        return tmpString;
+    }
+
+};
 //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+let myWord = new Word(wordToGuess);
+let whatsReturned = myWord.showPlaceholders();
+console.log("showPlaceholders function returned " + whatsReturned);
